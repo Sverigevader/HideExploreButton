8c35d516d8e6e41d0b78e1369ff7407be6cd166d
Sverigevader <1084623+Sverigevader@users.noreply.github.com>
2022-02-23 07:30:03 +0100

    added filter to hide the new Shorts button

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
 // ==UserScript==
 // @name     YouTube - Remove Explore button
-// @version  1
+// @description Hides the Explore button from YouTube
+// @version  3
 // @grant    none
 // @include	*://youtube.com/*
 // @include	*://*.youtube.com/*
 // @author	@sverigevader
+// @namespace https://greasyfork.org/users/692021
 // ==/UserScript==
 
 window.setTimeout(
-	function check() {
-		if (document.querySelector('[title="Explore"]')) {
-		  main();
-		}
-		window.setTimeout(check, 250);
-	}, 250
+    function check() {
+        if (document.querySelector('[title="Explore"]')) {
+            explore();
+        }
+        if (document.querySelector('[title="Shorts"]')) {
+            shorts();
+        }
+        window.setTimeout(check, 250);
+    }, 250
 );
 
-function main() {
+function explore() {
     var node = document.querySelector('[title="Explore"]');
+    node.style.display = "none";
+}
+
+function shorts() {
+    var node = document.querySelector('[title="Shorts"]');
     node.style.display = "none";
 }
